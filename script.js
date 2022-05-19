f8e5796380aaefcc2e488bd40d539fab187cb2f7
russelldnn <russelldnn@gmail.com>
2022-05-19 19:53:45 -0400

    added variables for confirmation added initial data collection prompts, found a source explaining the use of parseINT

--- script.js
+++ script.js
@@ -4,14 +4,38 @@ number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 commonlet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 capitallet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+let confirmCom;
+let confirmCap;
+let confirmNum;
+let confirmSpe;
 
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
+function GenPassword(){
+  promptLet = parseInt(prompt ("How many characters would you like your password to be (must be between 8 and 128)"));
+
+  if (!promptLet) {
+    alert ("Please enter a value to begin")
+  } else if (promptLet > 128 || promptLet < 8) {
+    alert ("The value has to be between 8 and 128")
+  } else {
+    confirmCom = confirm ("Want to add common letters?");
+    confirmCap = confirm ("want to add capital letters?");
+    confirmNum = confirm ("want to add numbers?");
+    confirmSpe = confirm ("want to add special characters?");
+  } 
+}
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
+  let password = GenPassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
