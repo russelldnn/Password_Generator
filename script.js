01f4e3088d112b79ab81ac36bda2b47ed6096ec9
russelldnn <russelldnn@gmail.com>
2022-05-19 19:25:32 -0400

    added all password variables to be used

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// Assignment code here
+// adding all variables
+special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+commonlet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+capitallet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
